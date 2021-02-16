ac763acd89d86f72af2f8bcc0340fcbddea6bc4f
yves <49711665+rwamucyo@users.noreply.github.com>
2021-02-16 10:45:21 +0200

    Revert "Revert "Revert "Update cachematrix.R"""
    This reverts commit 77326c11da276c543d4e58d806e8c93b1abc0151.

--- cachematrix.R
+++ cachematrix.R
@@ -2,28 +2,14 @@
 ## functions do
 
 ## Write a short comment describing this function
-library(MASS)
+
 makeCacheMatrix <- function(x = matrix()) {
-  inv <-NULL             #assign inverse to NULL
-  set <- function(y){
-              x <<-y
-              inv <<- NULL
-}
-  get <- function() {x}
-  setInverse <- function(inverse){inv <<- inverse}#function to obtain inverse function
-  getInverse <- function() {inv}
-  list( set= set, get= get,setInverse = setInverse,getInverse= getInverse)
+
 }
 
 
-cacheSolve <- function(x, ...) {               #get cache data
- 
-  if( is.null(inv)){                            #checking whether inverse in NULL
-    message("getting cached data")               
-    return(inv)                                 #return inverse value
-  }
-  mat <- x$get()
-  inv <- Solve(mat,....)
-  x$setinverse(inv)
-  inv
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
 }
